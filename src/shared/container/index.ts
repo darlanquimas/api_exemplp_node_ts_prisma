95efe3afbf8b3d68f03594abd89b7cb540766718
Darlan Quimas <darlanquimas@gmail.com>
2023-05-02 22:01:05 -0300

    ajuste nas rotas

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,30 +1,11 @@
-import { ICustomerRepository } from '@modules/customer/interfaces/ICustomerRepository';
 import { UsersRepository } from '@modules/users/infra/reporitosies/UsersRepository';
-import { CustomerRepository } from '@modules/customer/infra/reporitosies/CustomerRepository';
 import { IUsersRepository } from '@modules/users/interfaces/IUsersRepository';
 import { container } from 'tsyringe';
-import { ICustomerContactRepository } from '@modules/customer/interfaces/ICustomerContactRepository';
-import { CustomerContactRepository } from '@modules/customer/infra/reporitosies/CustomerContactRepository';
-import { CustomerAddressRepository } from '@modules/customer/infra/reporitosies/CustomerAddressRepository';
-import { ICustomerAddressRepository } from '@modules/customer/interfaces/ICustomerAddressRepository';
+
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
 
-container.registerSingleton<ICustomerRepository>(
-  'CustomerRepository',
-  CustomerRepository,
-);
-
-container.registerSingleton<ICustomerAddressRepository>(
-  'CustomerAddressRepository',
-  CustomerAddressRepository,
-);
-
-container.registerSingleton<ICustomerContactRepository>(
-  'CustomerContactRepository',
-  CustomerContactRepository,
-);
 export { container };
